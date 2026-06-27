5867c6c3eaea741ed25e75f0e72d0a2d5026ba1b
korneliacysewska <kornelia.cysewska2@wp.pl>
2026-06-27 03:58:57 +0200

    Fix CORS

--- BudgetApp/Program.cs
+++ BudgetApp/Program.cs
@@ -10,18 +10,26 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.InstanceName = "budgetapp:";
 });
 
-
 builder.Services.AddDbContext<BudgetContext>(options =>
     options.UseSqlServer(connectionString));
 
-
 builder.Services.AddControllers();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 
 if (app.Environment.IsDevelopment())
 {
@@ -29,7 +37,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
